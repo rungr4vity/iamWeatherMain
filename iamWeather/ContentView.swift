cbd85d47e9a9346e1cb7cbc2fdf7f56c2aeff45e
Francisco Martin <il.capitano.bam@gmail.com>
2023-12-25 22:52:50 -0600

    ok

--- iamWeather/ContentView.swift
+++ iamWeather/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, new world!")
+            Text("Hello, new world 2!")
         }
         .padding()
     }
